93a4807546fce144fc38d970d140669e9acad45f
ytconvertermp3 <yt.converter1.mp3@gmail.com>
2019-04-13 12:56:21 +0200

    update SEO
    text aangepast, meta tags toegevoegd, social links in js

---
+++ main.js
@@ -0,0 +1,11 @@
+function socialMediaT() {
+  window.open("https://twitter.com/youtubeconvert8");
+}
+
+function socialMediaF() {
+  window.open("https://www.facebook.com/yt.converter.716");
+}
+
+function socialMediaI() {
+  window.open("https://www.instagram.com/?hl=nl");
+}
